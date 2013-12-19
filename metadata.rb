5a783aea01416f65af6cbda6173587e769ed052c
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-12-19 13:04:55 +0100

    Fix cookbook name of lvm recipe and add missing recipe

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'm.benchchaoui@cloudbau.de'
 license          'All rights reserved'
 description      'Installs/Configures openstack-block-storage-wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'openstack-block-storage'
--- recipes/volume.rb
+++ recipes/volume.rb
@@ -1,7 +1,7 @@
 driver = node["openstack"]["block-storage"]["volume"]["driver"]
 
 if driver == "cinder.volume.drivers.lvm.LVMISCSIDriver"
-  include_recipe "openstack-block-storage::lvm"
+  include_recipe "openstack-block-storage-wrapper::lvm"
 else
   NotImplementedError.new("Unsupported driver #{driver}")
 end
